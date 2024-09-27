8523f211179c386c74b8f9b94c34803fe4c91680
master <nishanthgfg@gmail.com>
2024-09-27 14:25:21 -0700

    Initialized an express + TS app

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+import serverConfig from "./config/server.config"
+
+const app = express()
+
+
+app.listen(serverConfig.PORT,() => {
+
+ 
+  console.log(`The app has started running on PORT : ${serverConfig.PORT}`)
+
+})
